69f5ba660ae48da5145e2b22fb822cc146bc5bd5
GraceGenuardi <graziagenuardi96@gmail.com>
2023-01-30 14:59:40 +0100

    EDIT data struttura e azioni JS

--- js/app.js
+++ js/app.js
@@ -5,27 +5,27 @@ const titleElement = document.getElementById('first-title')
 
 
 
-titleElement.innerHTML = 'DaenerysTargaryenRed789'
-
-let nome
-let cognome
-let message = 'Benvenuto '
+titleElement.innerHTML = 'DaenerysTargaryenRed0000'
 
 
 
-// 1. chiedere nome all'utente
+/*****LINEA D'AZIONE */
+
+
+// 1. Chiedo all'utente il suo nome
 let name = prompt('Come vuoi che ti chiami?')
 console.log(name)
-// 2. chidere cognome all'utente
+
+// 2. Chiedo all'utente il suo cognome
 let username = prompt('Bene, e quando combini dei guai?')
 console.log(name)
 
 
-// 3. chiedere colore preferito
+// 3. Chiedo anche il suo colore preferito
 let color = prompt('Ottimo, e il tuo colore preferito?')
 console.log(name)
 
-// 4. chiedere un numero
-let number = prompt('Wow, e posso chiederti quanti hanni hai?')
+// 4. Gli chiedo un numero
+let number = prompt('Wow, e posso chiederti in che anno pensi di conquistare Westeros?')
 console.log(name)
 
