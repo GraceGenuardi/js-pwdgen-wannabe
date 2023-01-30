ee18a89cef8e039aa3080d2284f37d2cb4d8edf7
GraceGenuardi <graziagenuardi96@gmail.com>
2023-01-30 14:54:47 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log('prendo elemento html con id first-title')
+const titleElement = document.getElementById('first-title')
+
+
+
+
+
+titleElement.innerHTML = 'DaenerysTargaryenRed789'
+
+let nome
+let cognome
+let message = 'Benvenuto '
+
+
+
+// 1. chiedere nome all'utente
+let name = prompt('Come vuoi che ti chiami?')
+console.log(name)
+// 2. chidere cognome all'utente
+let username = prompt('Bene, e quando combini dei guai?')
+console.log(name)
+
+
+// 3. chiedere colore preferito
+let color = prompt('Ottimo, e il tuo colore preferito?')
+console.log(name)
+
+// 4. chiedere un numero
+let number = prompt('Wow, e posso chiederti quanti hanni hai?')
+console.log(name)
+
